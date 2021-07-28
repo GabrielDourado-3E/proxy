f284f6f62ce4690f52c22697184b863ca1affecf
Luciano Vilela <lucianovilela@gmail.com>
2021-07-27 23:08:29 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const morgan = require("morgan");
+const {
+  createProxyMiddleware,
+  responseInterceptor
+} = require("http-proxy-middleware");
+
+const app = express();
+// Logging
+app.use(morgan("dev"));
+// Info GET endpoint
+app.get("/info", (req, res, next) => {
+  res.send(
+    "This is a proxy service which proxies to Billing and Account APIs."
+  );
+});
+
+app.use(
+  "/api",
+  createProxyMiddleware({
+    logLevel: "debug",
+    //target: "https://jsonplaceholder.typicode.com",
+    target: "https://www.uol.com.br",
+    changeOrigin: true,
+    selfHandleResponse: true,
+    pathRewrite: {
+      [`^/api`]: ""
+    },
+
+    onProxyRes: responseInterceptor(
+      async (responseBuffer, proxyRes, req, res) => {
+        const response = responseBuffer.toString("utf8"); // convert buffer to string
+        return response.replace(/bolsonaro/gi, "Bozo"); // manipulate response and return the result
+      }
+    ),
+    onProxyReq: async (proxyRes, req, res) => {}
+  })
+);
+
+// // Authorization
+// app.use("", (req, res, next) => {
+//   if (req.headers.authorization) {
+//     next();
+//   } else {
+//     res.sendStatus(403);
+//   }
+// });
+
+app.listen(8080);
