f673f109fc4cc4aacf579513ea2e1fba66519c66
Luciano Vilela <lucianovilela@gmail.com>
2021-08-04 16:32:49 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -36,7 +36,7 @@ app.use(
       }
     ),
     */
-    onProxyReq:  (proxyRes, req, res) => {
+    onProxyReq: function (proxyRes, req, res) {
       proxyReq.setHeader("referrerPolicy", "strict-origin-when-cross-origin");
       proxyReq.setHeader("body", "null");
       proxyReq.setHeader("credentials", "omit");
