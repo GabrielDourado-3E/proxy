8d215cd233d974e683019cfbf339193161ad6c5d
GabrielDourado-3E <88496426+GabrielDourado-3E@users.noreply.github.com>
2021-08-05 11:11:12 -0300

    Update index.js
    Code cleaning

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
-const {
-  createProxyMiddleware,
-  responseInterceptor,
-  
-} = require("http-proxy-middleware");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const app = express();
 // Logging
@@ -12,7 +8,7 @@ app.use(morgan("dev"));
 // Info GET endpoint
 app.get("/info", (req, res, next) => {
   res.send(
-    "This is a proxy service which proxies to Billing and Account APIs."
+    "Tudo está funcionando!!"
   );
 });
 
@@ -20,49 +16,15 @@ app.use(
   "/",
   createProxyMiddleware({
     logLevel: "debug",
-    //target: "https://jsonplaceholder.typicode.com",
     target: {host:"docs.google.com", port:443, protocol:"https:", },
     changeOrigin: true,
-    //selfHandleResponse: true,
     autoRewrite : true,
     followRedirects : true,
-    pathRewrite: {
-      [`^/api`]: ""
-    },
-/*
-    onProxyRes: responseInterceptor(
-      async (responseBuffer, proxyRes, req, res) => {
-        const response = responseBuffer.toString("utf8"); // convert buffer to string
-        return response.replace(/bolsonaro/gi, "Bozo"); // manipulate response and return the result
-      }
-    ),
-    */
-
     
-
-    onProxyReq:  (proxyReq, req, res)=> {
-      /*
-      proxyReq.setHeader("referrerPolicy", "strict-origin-when-cross-origin");
-      proxyReq.setHeader("body", "null");
-      proxyReq.setHeader("credentials", "omit");
-      proxyReq.setHeader('mode', 'cors');
-      proxyReq.setHeader('host', 'docs.google.com');
-*/
-    },
-
     onError:(err, req, res, target)=>{
       console.log(err);
     }
   })
 );
-
-// // Authorization
-// app.use("", (req, res, next) => {
-//   if (req.headers.authorization) {
-//     next();
-//   } else {
-//     res.sendStatus(403);
-//   }
-// });
 
 app.listen(process.env.PORT || 8080);
